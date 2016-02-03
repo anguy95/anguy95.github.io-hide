a8b7e0bf60c9f3167e54b9c5af1825c7e108fae3
Andrew Nguyen <anguy95@gmail.com>
2016-02-02 23:09:44 -0800

    Added font awesome icons, and content. Need to add 1 project description, need to make mobile responsive now

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
   var bg;
   $(".skill-box").hover(
 
-  function() {
-    bg = $(this).css("background-image");
-    $(this).css("background-image", "none");
-    $(this).css("color", "white");
-  },
-  function() {
-    $(this).css("background-image", bg);
-    $(this).css("color", "rgba(0, 0, 0, 0)");
-  });
+    function() {
+      bg = $(this).css("background-image");
+      $(this).css("background-image", "none");
+      $(this).css("color", "white");
+    },
+    function() {
+      $(this).css("background-image", bg);
+      $(this).css("color", "rgba(0, 0, 0, 0)");
+    });
 });
